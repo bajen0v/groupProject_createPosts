bf67c638e383e6a932220459a8c86fa45ed07f6c
bajen0v <ibajenov1991@gmail.com>
2023-03-16 22:21:31 +0400

    Отредактировал код

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,24 +1,32 @@
-import s from './styles.module.css'
-
 import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+
+import s from './styles.module.css'
 
 export function Header() {
     return (
         <header className={s.header}>      
-           <AppBar position="static">
-           <Toolbar>
+          <AppBar position="static">
+            <Toolbar>
             <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-            Блог обо всем на свете
+              Блог обо всем на свете
             </Typography>
-           <Button variant="contained" 
-         onClick={() => {alert('контакт есть');}}>
-          Создать пост</Button> 
-         <Button variant="contained" 
-          onClick={() => {alert('логин работает');}}>
-          Login</Button> 
-         </Toolbar>
-       </AppBar>
-            
+            <Button 
+            variant = "contained" 
+            onClick = {() => {
+              alert('контакт есть');
+              }
+            }
+            >Создать пост
+            </Button> 
+            <Button 
+            variant="contained" 
+            onClick={() => {
+              alert('логин работает');
+              }
+            }>Login
+            </Button> 
+            </Toolbar>
+          </AppBar>  
         </header> 
     )
 }
