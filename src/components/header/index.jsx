f72a5e8bb9e411f4238a419762e0a1fe75e72925
Alla5735 <you@example.com>
2023-03-17 22:45:58 +0300

    Растягивает Header, добавляет footer

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,32 +1,25 @@
-import { AppBar, Button, Toolbar, Typography } from "@mui/material";
-
 import s from './styles.module.css'
+
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+
 
 export function Header() {
     return (
         <header className={s.header}>      
           <AppBar position="static">
-            <Toolbar>
-            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+           <Toolbar>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }} className={s.Typography}>
               Блог обо всем на свете
             </Typography>
-            <Button 
-            variant = "contained" 
-            onClick = {() => {
-              alert('контакт есть');
-              }
-            }
-            >Создать пост
+            <Button variant="contained" onClick={() => {alert('контакт есть');}}>
+             <AddIcon/>
             </Button> 
-            <Button 
-            variant="contained" 
-            onClick={() => {
-              alert('логин работает');
-              }
-            }>Login
+            <Button variant="contained" onClick={() => {alert('логин работает');}}>
+             Login
             </Button> 
-            </Toolbar>
-          </AppBar>  
+           </Toolbar>
+          </AppBar>
         </header> 
     )
 }
