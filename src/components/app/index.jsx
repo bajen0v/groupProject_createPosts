dcf2de6f4ece4e8e82a0ca6c97be5c42d71a74a4
bajen0v <ibajenov1991@gmail.com>
2023-03-20 21:18:21 +0400

    корректировка кода в файле дата

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,13 +1,8 @@
 import { CssBaseline } from '@mui/material';
 import { Container } from '@mui/system';
-import { Date } from '../date';
 import { Footer } from '../footer';
 import { Header } from '../header';
 import { PostList } from '../post-list';
-
-
-// import s from './styles.module.css';
-
 
 export function App() {
   return (
