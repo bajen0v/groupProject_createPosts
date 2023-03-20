e2dbf58c517f6de966cfe4c18e81818893aecb7b
Alla5735 <you@example.com>
2023-03-20 19:34:32 +0300

    Добавляет пагинацию

--- src/components/post-list/index.jsx
+++ src/components/post-list/index.jsx
@@ -1,13 +1,23 @@
 import { Grid } from '@mui/material';
-import {postData} from '../../posts';
+import { useState } from 'react';
+//import {postData} from '../../posts';
+import AppPagination from '../pagination';
 import { Post } from '../post';
 
 import s from './styles.module.css';
 
+
+
 export function PostList() {
+
+    const [postData, SetpostData] = useState([]);
+    
     return (
+        <>
         <Grid container spacing={4} className={s.content__posts}>
             {postData.map((dataItem, index) => <Post key={index} {...dataItem} />)}
         </Grid> 
+        <AppPagination SetpostData ={(p) => SetpostData(p)}/>
+        </>
     )
 }
