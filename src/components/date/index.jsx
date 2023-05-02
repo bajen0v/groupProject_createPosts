ae10e53c0d8c24d15428a08b9f087a5a3bb77dba
bajen0v <ibajenov1991@gmail.com>
2023-05-02 22:43:01 +0400

    пофиксил дату, добавил условие, работает корректно

--- src/components/date/index.jsx
+++ src/components/date/index.jsx
@@ -1,18 +1,22 @@
 import dayjs from "dayjs";
 import relativeTime from 'dayjs/plugin/relativeTime';
+import objectSupport from 'dayjs/plugin/objectSupport';
 import 'dayjs/locale/ru';
 dayjs.locale('ru');
 dayjs.extend(relativeTime);
+// dayjs.extend(objectSupport)
 
 export function Date({ created_at }) {
-  
   const dateCreated = dayjs(created_at);
   const dayCreated = dateCreated.date();
+  const monthCreated = dateCreated.month();
+
   const today = dayjs().date()
+  const todayMonth = dayjs().month()
   
-  return dayCreated === today
+  return dayCreated === today && monthCreated === todayMonth
     ? "Сегодня " + dateCreated.format("HH:mm")
-    : today - dayCreated === 1
+    : today - dayCreated === 1 && monthCreated === todayMonth
         ? "Вчера " + dateCreated.format("hh:mm") 
         : dateCreated.format("DD MMMM YYYY") + " г.";
 }
